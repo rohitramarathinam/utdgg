bd087486dfe5d319051e7e1ac4d190e007833d5d
axk4821 <akokala47@gmail.com>
2021-12-31 18:49:01 -0600

    Created dictionary that holds percent of studnents that got a specific grade based on the professor they had.

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ searchForm.addEventListener("submit", function(e){
     var dataParsed = JSON.parse(httpGet(`https://4i5536aptc.execute-api.us-east-2.amazonaws.com/section?search=${search}&sortDirection=DESC&sortField=year`));
     var allData = []
     var dataByProf = {}
+    var percentDataByProf = {}
+    var allGrades = []
+
     for (let index = 0; index < dataParsed.length; index++) {
         var dataByTerm = {};
 
@@ -51,5 +54,32 @@ searchForm.addEventListener("submit", function(e){
         // Data is organized by professor
         try{dataByProf[dataByTerm['prof']].push(dataByTerm['grades']);} catch(e){if (!(dataByProf[dataByTerm['prof']])) {dataByProf[dataByTerm['prof']] = [dataByTerm['grades']];};}
     }
-    console.log(dataByProf)
+    
+    // Consolidates data by professor
+    for (var prof in dataByProf){
+        percentDataByProf[prof] = {}
+        for (let grades = 0; grades < dataByProf[prof].length; grades++){
+            for (var grade in dataByProf[prof][grades]){
+                if (!(percentDataByProf[prof][grade])){
+                    allGrades.push(grade);
+                    percentDataByProf[prof][grade] = dataByProf[prof][grades][grade];
+                }
+                else{
+                    percentDataByProf[prof][grade] += dataByProf[prof][grades][grade];
+                }
+            }
+        }
+    }
+
+    // Removes Duplicates from allGrades
+
+    allGrades = [...new Set(allGrades)]
+
+    // Data is organized by professor and the percent students that recieved each grade
+    for (prof in percentDataByProf){
+        var totalPeople = sum(percentDataByProf[prof])
+        for (grade in percentDataByProf[prof]){
+            percentDataByProf[prof][grade] = (percentDataByProf[prof][grade]*100.0)/totalPeople 
+        }
+    }
 })
